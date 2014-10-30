19e2a4df166533a9fb0b2074e1ab2d30f068ecf5
Daniel Danilatos <daniel@helixta.com.au>
2014-10-30 14:03:53 +1100

    decouple demos and dev server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  entry: '../client/injector.js',
+  entry: './injector.js',
   output: {
     path: __dirname + '/build',
     filename: 'copyraptor.js',
-    publicPath: '/build/',
+    publicPath: 'http://localhost:5544/build/',
 
     sourceMapFilename: '[file].map',
-
   }
 };
