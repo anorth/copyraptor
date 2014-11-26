6d601dc9135a7a5a873032b0d98eb6d6d6221899
Alex North <alex@alexn.id.au>
2014-11-26 11:13:35 +1100

    Added putSite to datastore; more datastore unit tests

--- server/datastore.js
+++ server/datastore.js
@@ -3,9 +3,13 @@ var Q = require('q');
 
 pg.defaults.poolSize = 16; // Heroku hobby limit is 20, leave a few for admin.
 
+/**
+ * All methods return promises.
+ */
 exports.Datastore = function Datastore(url) {
   var me = this;
 
+  /** Returns true if a simple query succeeds. */
   me.testConnection = function() {
     return oneshot('SELECT COUNT(*) FROM site')
         .then(function(result) {
@@ -13,6 +17,23 @@ exports.Datastore = function Datastore(url) {
         });
   };
 
+  /** Inserts a site owned by a new user. */
+  me.putSite = function(sitekey, password, email, username) {
+    var tx = transaction();
+    return tx.begin().then(function() {
+      return tx.query('INSERT INTO account (email, name) VALUES ($1, $2) RETURNING id', [email, username]);
+    }).then(function(result) {
+      var id = result.rows[0]['id'];
+      //console.log('Account created with id ' + id);
+      return tx.query('INSERT INTO site (sitekey, password, owner) VALUES ($1, crypt($2, gen_salt(\'bf\')), $3)',
+          [sitekey, password, id]);
+    }).then(function(result) {
+      console.log("New site", sitekey, email);
+      return tx.commit();
+    });
+  };
+
+  /** Fetches a site by key. */
   me.getSite = function(sitekey) {
     return oneshot('SELECT * FROM site WHERE sitekey = $1', [sitekey])
         .then(function(result) {
@@ -24,7 +45,7 @@ exports.Datastore = function Datastore(url) {
         });
   };
 
-  /** Returns a promise of a boolean indicating login success. */
+  /** Checks login against a site password. */
   me.checkLogin = function(site, password) {
     return oneshot('SELECT * FROM site_with_owner WHERE sitekey = $1 AND password = crypt($2, password)', [site, password])
         .then(function(result) {
@@ -71,6 +92,75 @@ exports.Datastore = function Datastore(url) {
             });
           });
         });
+  }
+
+  function transaction() {
+    var qConnect;
+
+    function begin(client, done) {
+      return Q.promise(function(resolve, reject) {
+        client.query('BEGIN', function(err) {
+          if (!err) {
+            resolve();
+          } else {
+            done(err);
+            reject(err);
+          }
+        });
+      });
+    }
+
+    function rollback(client, done) {
+      return Q.promise(function(resolve, reject) {
+        client.query('ROLLBACK', function(err) {
+          done(err);
+          if (!err) resolve(); else reject(err);
+        });
+      });
+    }
+
+    function commit(client, done) {
+      return Q.promise(function(resolve, reject) {
+        client.query('COMMIT', function(err) {
+          done(err);
+          if (!err) resolve(true); else reject(err);
+        });
+      });
+    }
+
+    var tx = {};
+
+    // Returns a promise satisfied when connected.
+    tx.begin = function() {
+      qConnect = connect();
+      return qConnect.spread(function(client, done) {
+            return begin(client, done);
+          });
+    };
+
+    tx.query = function(query, params) {
+      return qConnect.spread(function(client, done) {
+        return Q.promise(function(resolve, reject) {
+          client.query(query, params, function(err, result) {
+            if (!err) {
+              resolve(result);
+            } else {
+              reject(err);
+            }
+          });
+        })
+      });
+    };
+
+    tx.commit = function() {
+      return qConnect.spread(commit);
+    };
+
+    tx.rollback = function() {
+      return qConnect.spread(rollback);
+    };
+
+    return tx;
   }
 };
 
--- server/datastore.spec.js
+++ server/datastore.spec.js
@@ -18,13 +18,65 @@ describe('Datastore tests', function () {
 
   it('should connect successfully', promised(function () {
     return store.then(function(store) {
-      store.testConnection()
+      return store.testConnection()
           .then(function(ok) {
             expect(ok).toBeTruthy();
           });
     });
   }));
+
+  it('should add a new site', promised(function () {
+    return store.then(function(store) {
+      return store.putSite('site', 'password', 'email@example.com', 'User Name')
+          .then(function(ok) {
+            expect(ok).toBeTruthy();
+          }).then(function() {
+            return store.getSite('site');
+          }).then(function(site) {
+            expect(site.sitekey).toEqual('site');
+            expect(site.password).toBeTruthy();
+            expect(site.owner).toBeTruthy();
+          });
+    });
+  }));
+
+  it('should reject duplicate site key', promised(function () {
+    return store.then(function(store) {
+      return store.putSite('site', 'password', 'email@example.com', 'User Name')
+          .then(function(ok) {
+            expect(ok).toBeTruthy();
+            return store.putSite('site', 'pass', 'some@exmple.com', 'Who')
+          }).then(function(ok) {
+            fail();
+          }).catch(function(err) {
+            expect(err.code).toEqual("23505");
+          });
+          // TODO(alex): check no user account created, i.e. transactional
+    });
+  }));
+
+
+  it('should check site password', promised(function () {
+    return store.then(function(store) {
+      return store.putSite('site', 'password', 'email@example.com', 'User Name')
+          .then(function(ok) {
+            expect(ok).toBeTruthy();
+          }).then(function() {
+            return store.checkLogin('site', 'xxx');
+          }).then(function(ok) {
+            expect(ok).toBe(false);
+          }).then(function() {
+            return store.checkLogin('site', 'password')
+          }).then(function(ok) {
+            expect(ok).toBe(true);
+          });
+    });
+  }));
 });
+
+function fail() {
+  expect(false).toBeTruthy();
+}
 
 
 function promised(func) {
